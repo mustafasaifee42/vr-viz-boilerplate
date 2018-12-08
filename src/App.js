a9246d3591e8872ba5a02790e307a2636525e6f7
Mustafa Saifee <saifee.mustafa@gmail.com>
2018-12-08 12:01:54 +0200

    creating boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,153 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import VRViz from "vr-viz";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <VRViz
+        scene={
+          {
+            'sky': {
+              'style': {
+                'color': '#333',
+                'texture': false,
+              }
+            },
+            'lights': [
+              {
+                'type': 'directional',
+                'color': '#fff',
+                'position': '0 1 1',
+                'intensity': 1,
+                'decay': 1,
+              },
+              {
+                'type': 'ambient',
+                'color': '#fff',
+                'intensity': 1,
+                'decay': 1,
+              }
+            ],
+            'camera': {
+              'position': '10 0 20',
+              'rotation': '0 0 0',
+            },
+          }
+        }
+        graph={
+          [
+            {
+              'type': 'BarGraph',
+              'data': {
+                'dataFile': 'data.csv',
+                'fileType': 'csv',
+                'fieldDesc': [['x', 'text'], ['z', 'text'], ['height', 'number']]
+              },
+              'style': {
+                'origin': [0, 0, 0],
+                'dimensions': {
+                  'width': 20,
+                  'height': 5,
+                  'depth': 10,
+                },
+              },
+              'mark': {
+                'type': 'box',
+                'position': {
+                  'x': {
+                    'scaleType': 'ordinal',
+                    'field': 'x',
+                  },
+                  'z': {
+                    'scaleType': 'ordinal',
+                    'field': 'z',
+                  }
+                },
+                'style': {
+                  'padding': {
+                    'x': 0.1,
+                    'z': 0.1,
+                  },
+                  'height': {
+                    'scaleType': 'linear',
+                    'startFromZero': true,
+                    'field': 'height',
+                  },
+                  'fill': {
+                    'opacity': 0.8,
+                    'scaleType': 'linear',
+                    'field': 'height',
+                    'color': ['#b71c1c', '#2196f3'],
+                  },
+                },
+              },
+              'axis': {
+                'x-axis': {
+                  'orient': 'back-bottom',
+                  'title': {
+                    'value': '',
+                    'fontSize': 3,
+                    'color': 'white',
+                    'opacity': 0.7,
+                  },
+                  'ticks': {
+                    'noOfTicks': 10,
+                    'size': 0.01,
+                    'color': 'white',
+                    'opacity': 0.7,
+                    'fontSize': 3,
+                  },
+                  'grid': {
+                    'color': 'white',
+                    'opacity': 0.7,
+                  }
+                },
+                'y-axis': {
+                  'orient': 'back-left',
+                  'title': {
+                    'value': '',
+                    'fontSize': 3,
+                    'color': 'white',
+                    'opacity': 0.7,
+                  },
+                  'ticks': {
+                    'noOfTicks': 10,
+                    'size': 0.01,
+                    'color': 'white',
+                    'opacity': 0.7,
+                    'fontSize': 3,
+                  },
+                  'grid': {
+                    'color': 'white',
+                    'opacity': 0.7,
+                  }
+                },
+                'z-axis': {
+                  'orient': 'bottom-left',
+                  'title': {
+                    'value': '',
+                    'fontSize': 3,
+                    'color': 'white',
+                    'opacity': 0.7,
+                  },
+                  'ticks': {
+                    'noOfTicks': 10,
+                    'size': 0.01,
+                    'color': 'white',
+                    'opacity': 0.7,
+                    'fontSize': 3,
+                  },
+                  'grid': {
+                    'color': 'white',
+                    'opacity': 0.7,
+                  }
+                }
+              }
+            }
+          ]
+        }
+    />
     );
   }
 }
