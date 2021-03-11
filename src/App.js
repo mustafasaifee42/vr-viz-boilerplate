ab65020edf1b0c1cc20bcc237083b08a74b6ad5a
mustafasaifee42 <saifee.mustafa@gmail.com>
2021-03-11 02:39:52 +0200

    update react and vr-viz version

--- src/App.js
+++ src/App.js
@@ -1,155 +1,126 @@
-import React, { Component } from 'react';
 import VRViz from "vr-viz";
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <VRViz
-        scene={
+function App() {
+  return (
+    <VRViz
+      scene={{
+        sky: {
+          style: {
+            color: "#333",
+            texture: false,
+          },
+        },
+        lights: [
           {
-            'sky': {
-              'style': {
-                'color': '#333',
-                'texture': false,
-              }
-            },
-            'lights': [
-              {
-                'type': 'directional',
-                'color': '#fff',
-                'position': '0 1 1',
-                'intensity': 1,
-                'decay': 1,
-              },
-              {
-                'type': 'ambient',
-                'color': '#fff',
-                'intensity': 1,
-                'decay': 1,
-              }
+            type: "directional",
+            color: "#fff",
+            position: "0 1 1",
+            intensity: 1,
+            decay: 1,
+          },
+          {
+            type: "ambient",
+            color: "#fff",
+            intensity: 1,
+            decay: 1,
+          },
+        ],
+        camera: {
+          position: "10 0 20",
+          rotation: "0 0 0",
+        },
+      }}
+      graph={[
+        {
+          type: "BarGraph",
+          data: {
+            dataFile: "data.csv",
+            fileType: "csv",
+            fieldDesc: [
+              ["x", "text"],
+              ["z", "text"],
+              ["height", "number"],
             ],
-            'camera': {
-              'position': '10 0 20',
-              'rotation': '0 0 0',
+          },
+          style: {
+            origin: [0, 0, 0],
+            dimensions: {
+              width: 20,
+              height: 5,
+              depth: 10,
             },
-          }
-        }
-        graph={
-          [
-            {
-              'type': 'BarGraph',
-              'data': {
-                'dataFile': 'data.csv',
-                'fileType': 'csv',
-                'fieldDesc': [['x', 'text'], ['z', 'text'], ['height', 'number']]
+          },
+          mark: {
+            type: "box",
+            position: {
+              x: {
+                field: "x",
               },
-              'style': {
-                'origin': [0, 0, 0],
-                'dimensions': {
-                  'width': 20,
-                  'height': 5,
-                  'depth': 10,
-                },
+              z: {
+                field: "z",
               },
-              'mark': {
-                'type': 'box',
-                'position': {
-                  'x': {
-                    'scaleType': 'ordinal',
-                    'field': 'x',
-                  },
-                  'z': {
-                    'scaleType': 'ordinal',
-                    'field': 'z',
-                  }
-                },
-                'style': {
-                  'padding': {
-                    'x': 0.1,
-                    'z': 0.1,
-                  },
-                  'height': {
-                    'scaleType': 'linear',
-                    'startFromZero': true,
-                    'field': 'height',
-                  },
-                  'fill': {
-                    'opacity': 0.8,
-                    'scaleType': 'linear',
-                    'field': 'height',
-                    'color': ['#b71c1c', '#2196f3'],
-                  },
-                },
+            },
+            style: {
+              height: {
+                startFromZero: true,
+                field: "height",
               },
-              'axis': {
-                'x-axis': {
-                  'orient': 'back-bottom',
-                  'title': {
-                    'value': '',
-                    'fontSize': 3,
-                    'color': 'white',
-                    'opacity': 0.7,
-                  },
-                  'ticks': {
-                    'noOfTicks': 10,
-                    'size': 0.01,
-                    'color': 'white',
-                    'opacity': 0.7,
-                    'fontSize': 3,
-                  },
-                  'grid': {
-                    'color': 'white',
-                    'opacity': 0.7,
-                  }
-                },
-                'y-axis': {
-                  'orient': 'back-left',
-                  'title': {
-                    'value': '',
-                    'fontSize': 3,
-                    'color': 'white',
-                    'opacity': 0.7,
-                  },
-                  'ticks': {
-                    'noOfTicks': 10,
-                    'size': 0.01,
-                    'color': 'white',
-                    'opacity': 0.7,
-                    'fontSize': 3,
-                  },
-                  'grid': {
-                    'color': 'white',
-                    'opacity': 0.7,
-                  }
-                },
-                'z-axis': {
-                  'orient': 'bottom-left',
-                  'title': {
-                    'value': '',
-                    'fontSize': 3,
-                    'color': 'white',
-                    'opacity': 0.7,
-                  },
-                  'ticks': {
-                    'noOfTicks': 10,
-                    'size': 0.01,
-                    'color': 'white',
-                    'opacity': 0.7,
-                    'fontSize': 3,
-                  },
-                  'grid': {
-                    'color': 'white',
-                    'opacity': 0.7,
-                  }
-                }
-              }
-            }
-          ]
-        }
+              fill: {
+                opacity: 0.8,
+                scaleType: "linear",
+                field: "height",
+                color: ["#b71c1c", "#2196f3"],
+              },
+            },
+          },
+          axis: {
+            "x-axis": {
+              orient: "back-bottom",
+              ticks: {
+                noOfTicks: 10,
+                size: 0.01,
+                color: "white",
+                opacity: 0.7,
+                fontSize: 3,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.7,
+              },
+            },
+            "y-axis": {
+              orient: "back-left",
+              ticks: {
+                noOfTicks: 10,
+                size: 0.01,
+                color: "white",
+                opacity: 0.7,
+                fontSize: 3,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.7,
+              },
+            },
+            "z-axis": {
+              orient: "bottom-left",
+              ticks: {
+                noOfTicks: 10,
+                size: 0.01,
+                color: "white",
+                opacity: 0.7,
+                fontSize: 3,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.7,
+              },
+            },
+          },
+        },
+      ]}
     />
-    );
-  }
+  );
 }
 
 export default App;
